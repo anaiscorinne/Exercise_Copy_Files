50ae284ccdb20d2d1ac3697e77ca949a23643d5f
Anais Engelmajer <anaiscorinne@gmail.com>
2015-12-02 10:17:27 -0500

    Change lines of code to destination_file and source_file to try and fix a bug

--- exercise.rb
+++ exercise.rb
@@ -3,4 +3,4 @@ source_file = gets.chomp
 puts "What will the destination file be called?"
 destination_file = gets.chomp
 source_file_contents = IO.read(source_file)
-IO.write(file_name, file_contents)
+IO.write(destination_file, source_file_contents)
